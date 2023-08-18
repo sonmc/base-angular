6c4d6862e960fd53ef66465fbdba019258ee08b8
peter_m <sonmc90@gmail.com>
2023-08-18 17:15:11 +0700

    init

---
+++ src/polyfills.ts
@@ -0,0 +1,5 @@
+ 
+import '@angular/localize/init'; 
+import 'zone.js';   
+
+ 
---
+++ src/typings.d.ts
@@ -0,0 +1,5 @@
+declare var ClipboardJS: any;
+declare module '@ckeditor/ckeditor5-build-classic' {
+  const ClassicEditorBuild: any;
+  export default ClassicEditorBuild;
+}
